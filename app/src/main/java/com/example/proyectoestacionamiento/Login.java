2e72d50c8edf237dcf729baf463bb88c72e543b0
lalosantos3 <santoslirae@gmail.com>
2023-07-23 16:40:11 -0600

    Login 2 Tipos De Usuario

--- app/src/main/java/com/example/proyectoestacionamiento/Login.java
+++ app/src/main/java/com/example/proyectoestacionamiento/Login.java
@@ -1,24 +1,74 @@
 package com.example.proyectoestacionamiento;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-public class Login extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    Button B_Ingresar;
-    EditText E_Usu, E_Cont;
+public class Login extends AppCompatActivity {
+    private EditText editTextUsername;
+    private EditText editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        E_Usu = findViewById(R.id.E_Usu);
-        E_Cont = findViewById(R.id.E_Cont);
-        B_Ingresar = findViewById(R.id.B_Ingresar);
+        editTextUsername = findViewById(R.id.Eusu);
+        editTextPassword = findViewById(R.id.Econt);
+        Button buttonIngresar = findViewById(R.id.Bingresar);
 
+        buttonIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Obtener los datos de usuario y contraseña ingresados por el usuario
+                String username = editTextUsername.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                // Validar que los campos no estén vacíos
+                if (username.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(Login.this, "Ingrese usuario y contraseña", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Realizar la solicitud HTTP para el inicio de sesión
+                    LoginTask loginTask = new LoginTask(new LoginTask.OnLoginResultListener() {
+                        @Override
+                        public void onLoginResult(String result) {
+                            // Procesar la respuesta del servidor (inicio de sesión exitoso o fallido)
+                            try {
+                                JSONObject jsonObject = new JSONObject(result);
+                                String status = jsonObject.getString("status");
+                                String message = jsonObject.getString("message");
+                                String rol = jsonObject.getString("rol");
+
+                                if (status.equals("success")) {
+                                    Toast.makeText(Login.this, message, Toast.LENGTH_SHORT).show();
+                                    // Redirigir al usuario según el rol
+                                    if (rol.equalsIgnoreCase("Admin")) {
+                                        Intent intent = new Intent(Login.this, MenuAdmin.class);
+                                        startActivity(intent);
+                                    } else if (rol.equalsIgnoreCase("Usuario")) {
+                                        Intent intent = new Intent(Login.this, Menu.class);
+                                        startActivity(intent);
+                                    }
+                                    // Finalizar la actividad actual (opcional, dependiendo de la lógica de la app)
+                                    finish();
+                                } else {
+                                    Toast.makeText(Login.this, message, Toast.LENGTH_SHORT).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                    loginTask.execute(username, password);
+                }
+            }
+        });
     }
-
 }
---
+++ app/src/main/java/com/example/proyectoestacionamiento/LoginTask.java
@@ -0,0 +1,65 @@
+package com.example.proyectoestacionamiento;
+
+import android.os.AsyncTask;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class LoginTask extends AsyncTask<String, Void, String> {
+
+    public interface OnLoginResultListener {
+        void onLoginResult(String result);
+    }
+
+    private OnLoginResultListener listener;
+
+    public LoginTask(OnLoginResultListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String urlString = "https://estacionamientohmagdl.000webhostapp.com/Estacionamiento/Login/login.php";
+        String username = params[0];
+        String password = params[1];
+        String response = "";
+
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+
+            // Construir el cuerpo de la solicitud
+            String body = "username=" + username + "&password=" + password;
+            OutputStream outputStream = connection.getOutputStream();
+            outputStream.write(body.getBytes());
+            outputStream.flush();
+            outputStream.close();
+
+            // Leer la respuesta del servidor
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response += line;
+            }
+            reader.close();
+
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return response;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        if (listener != null) {
+            listener.onLoginResult(result);
+        }
+    }
+}
