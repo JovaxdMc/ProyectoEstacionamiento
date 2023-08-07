7bb7ce55c7c340bb9e7a7f220415df2952a2b488
David-G-Q <2030292@upt.edu.mx>
2023-08-07 13:25:32 -0600

    Ventiladores funcion

--- app/src/main/java/com/example/proyectoestacionamiento/Ventiladores.java
+++ app/src/main/java/com/example/proyectoestacionamiento/Ventiladores.java
@@ -3,12 +3,98 @@ package com.example.proyectoestacionamiento;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Ventiladores extends AppCompatActivity {
+
+    LinearLayout btnOn,btnOf,Diez,Treinta,cincuenta,ochenta,cien;
+    EditText Vel;
+    TextView textOn,textOf;
+    Button Guardar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ventiladores);
+
+        btnOn = findViewById(R.id.btnOn);
+        btnOf = findViewById(R.id.btnOf);
+        Vel = findViewById(R.id.Vel);
+        Guardar = findViewById(R.id.Guardar);
+        textOn = findViewById(R.id.textOn);
+        textOf = findViewById(R.id.textOf);
+
+
+        Guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Modificar("https://estacionamientohmagdl.000webhostapp.com/Estacionamiento/Actuadores/ventiladores.php");
+            }
+        });
+
+        btnOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Modificar("https://estacionamientohmagdl.000webhostapp.com/Estacionamiento/Actuadores/ventiladores.php");
+            }
+        });
+        btnOf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Modificar("https://estacionamientohmagdl.000webhostapp.com/Estacionamiento/Actuadores/ventiladores.php");
+            }
+        });
+
+
     }
+
+    public void Modificar(String url) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getApplicationContext(), "Registro actualizado", Toast.LENGTH_LONG).show();
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("velocidad", Vel.getText().toString());
+                params.put("estado", textOn.getText().toString());
+                params.put("estado", textOf.getText().toString());
+
+
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
+    }
+
+
+
+
 }
