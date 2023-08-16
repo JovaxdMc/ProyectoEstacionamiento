f97a55c9883af8468ef0c79e271d8d34e761f12c
David-G-Q <2030292@upt.edu.mx>
2023-08-15 20:05:12 -0600

    ventilador y temperatura llamr datos listo

--- app/src/main/java/com/example/proyectoestacionamiento/Temperatura.java
+++ app/src/main/java/com/example/proyectoestacionamiento/Temperatura.java
@@ -1,14 +1,98 @@
 package com.example.proyectoestacionamiento;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Temperatura extends AppCompatActivity {
+    EditText txtnumer;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temperatura);
+
+        txtnumer = findViewById(R.id.txtnumer);
+        handler = new Handler();
+
+        // Ejecutar la obtención de temperatura automáticamente al entrar
+        obtenerTemperaturaTermometro();
+
+        // Ejecutar la obtención de temperatura automáticamente cada 5 segundos
+        handler.postDelayed(obtenerTemperaturaRunnable, 5000);
+    }
+
+    private Runnable obtenerTemperaturaRunnable = new Runnable() {
+        @Override
+        public void run() {
+            obtenerTemperaturaTermometro();
+
+            // Ejecutar nuevamente el Runnable después de 5 segundos
+            handler.postDelayed(this, 5000);
+        }
+    };
+
+    private void obtenerTemperaturaTermometro() {
+        // URL de la API para obtener los datos del termómetro
+        String url = "https://estacionamientohmagdl.000webhostapp.com/Estacionamiento/Actuadores/termometro.php";
+
+        // Crear una nueva solicitud POST con Volley
+        StringRequest request = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Procesar la respuesta JSON y actualizar la vista con la temperatura
+                        try {
+                            JSONObject datosJson = new JSONObject(response);
+                            double temperatura = datosJson.getDouble("temperatura");
+
+                            // Actualizar la vista con la temperatura obtenida
+                            txtnumer.setText(String.valueOf(temperatura) + " °C");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Manejar el error en caso de que la solicitud no se pueda completar
+                        Toast.makeText(getApplicationContext(), "Error al obtener los datos del termómetro", Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("obtener_datos", ""); // Agregar el parámetro para indicar que deseas obtener los datos
+
+                return params;
+            }
+        };
+
+        // Agregar la solicitud a la cola de Volley para ejecutarla
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Detener el ciclo de obtención de temperatura al cerrar la actividad
+        handler.removeCallbacks(obtenerTemperaturaRunnable);
     }
 }
--- app/src/main/java/com/example/proyectoestacionamiento/Ventiladores.java
+++ app/src/main/java/com/example/proyectoestacionamiento/Ventiladores.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.InputFilter;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,18 +17,21 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class Ventiladores extends AppCompatActivity {
 
-    LinearLayout btnOn,btnOf,Diez,Treinta,cincuenta,ochenta,cien;
+    LinearLayout btnOn, btnOf;
     EditText Vel;
-    TextView textOn,textOf;
+    TextView textOn, textOf;
     Button Guardar;
 
     @Override
@@ -35,6 +39,7 @@ public class Ventiladores extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ventiladores);
 
+        // Inicializar vistas
         btnOn = findViewById(R.id.btnOn);
         btnOf = findViewById(R.id.btnOf);
         Vel = findViewById(R.id.Vel);
@@ -42,6 +47,9 @@ public class Ventiladores extends AppCompatActivity {
         textOn = findViewById(R.id.textOn);
         textOf = findViewById(R.id.textOf);
 
+        obtenerEstadoVentilador();
+
+        // Aplicar filtro de entrada para el campo de velocidad
         Vel.setFilters(new InputFilter[]{(source, start, end, dest, dstart, dend) -> {
             String newValue = dest.toString().substring(0, dstart) + source.toString() + dest.toString().substring(dend);
             if (newValue.isEmpty() || newValue.equals("-")) {
@@ -49,10 +57,10 @@ public class Ventiladores extends AppCompatActivity {
             }
             try {
                 if (newValue.equals("0")) {
-                    return null;  // Allow single zero
+                    return null;  // Permitir solo cero individual
                 }
                 if (newValue.startsWith("00")) {
-                    return "";  // Prevent multiple leading zeros
+                    return "";  // Evitar múltiples ceros iniciales
                 }
                 int input = Integer.parseInt(newValue);
                 return (input >= 0 && input <= 100) ? null : "";
@@ -62,6 +70,7 @@ public class Ventiladores extends AppCompatActivity {
             }
         }});
 
+        // Configurar botones y su comportamiento
         Guardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -82,8 +91,9 @@ public class Ventiladores extends AppCompatActivity {
                 Modificar3("https://estacionamientohmagdl.000webhostapp.com/Estacionamiento/Actuadores/ventiladores.php");
             }
         });
-    }
 
+
+    }
 
     public void Modificar(String url) {
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
@@ -91,7 +101,8 @@ public class Ventiladores extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
                         Toast.makeText(getApplicationContext(), "Registro actualizado", Toast.LENGTH_LONG).show();
-
+                        // Después de actualizar, obtener y mostrar los nuevos datos
+                        obtenerEstadoVentilador();
                     }
                 },
                 new Response.ErrorListener() {
@@ -104,7 +115,6 @@ public class Ventiladores extends AppCompatActivity {
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String, String> params = new HashMap<String, String>();
                 params.put("velocidad", Vel.getText().toString());
-
 
                 return params;
             }
@@ -119,7 +129,8 @@ public class Ventiladores extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
                         Toast.makeText(getApplicationContext(), "Registro actualizado", Toast.LENGTH_LONG).show();
-
+                        // Después de actualizar, obtener y mostrar los nuevos datos
+                        obtenerEstadoVentilador();
                     }
                 },
                 new Response.ErrorListener() {
@@ -131,8 +142,7 @@ public class Ventiladores extends AppCompatActivity {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String, String> params = new HashMap<String, String>();
-                params.put("estado", textOn.getText().toString());
-
+                params.put("estado", "On"); // Cambiar "On" por el valor deseado
 
                 return params;
             }
@@ -147,7 +157,8 @@ public class Ventiladores extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
                         Toast.makeText(getApplicationContext(), "Registro actualizado", Toast.LENGTH_LONG).show();
-
+                        // Después de actualizar, obtener y mostrar los nuevos datos
+                        obtenerEstadoVentilador();
                     }
                 },
                 new Response.ErrorListener() {
@@ -159,8 +170,7 @@ public class Ventiladores extends AppCompatActivity {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String, String> params = new HashMap<String, String>();
-                params.put("estado", textOf.getText().toString());
-
+                params.put("estado", "Off"); // Cambiar "Off" por el valor deseado
 
                 return params;
             }
@@ -169,6 +179,54 @@ public class Ventiladores extends AppCompatActivity {
         requestQueue.add(stringRequest);
     }
 
+    private void obtenerEstadoVentilador() {
+        // URL de la API para obtener los datos del ventilador
+        String url = "https://estacionamientohmagdl.000webhostapp.com/Estacionamiento/Actuadores/ventiladores.php";
 
+        // Crear una nueva solicitud POST con Volley
+        StringRequest request = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Procesar la respuesta JSON y actualizar las vistas con los datos del ventilador
+                        try {
+                            JSONObject datosJson = new JSONObject(response);
+                            int velocidad = datosJson.getInt("velocidad");
+                            String estado = datosJson.getString("estado");
 
+                            // Actualizar las vistas con los datos obtenidos
+                            Vel.setText(String.valueOf(velocidad));
+
+                            if (estado.equals("On")) {
+                                textOn.setTextColor(getResources().getColor(R.color.colorOn)); // Cambia R.color.colorOn por el color deseado
+                                textOf.setTextColor(getResources().getColor(R.color.colorOff)); // Cambia R.color.colorDefault por el color deseado
+                            } else {
+                                textOn.setTextColor(getResources().getColor(R.color.colorOff)); // Cambia R.color.colorDefault por el color deseado
+                                textOf.setTextColor(getResources().getColor(R.color.colorOn)); // Cambia R.color.colorOff por el color deseado
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Manejar el error en caso de que la solicitud no se pueda completar
+                        Toast.makeText(getApplicationContext(), "Error al obtener los datos del ventilador", Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("obtener_datos", ""); // Agregar el parámetro para indicar que deseas obtener los datos
+
+                return params;
+            }
+        };
+
+        // Agregar la solicitud a la cola de Volley para ejecutarla
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
+    }
 }
